990b1d7fad662c490d701bbdf67a95108abb3d6b
Dominic Egginton <dominic.egginton@gmail.com>
2021-01-06 17:45:46 +0000

    added timeinterval init mehtods to easy convert common time units

--- Sources/Nanoseconds/Nanoseconds.swift
+++ Sources/Nanoseconds/Nanoseconds.swift
@@ -22,6 +22,12 @@ extension Nanoseconds: Comparable {
 }
 
 extension TimeInterval {
+    public init (nanoseconds: Double) { self = nanoseconds }
+    public init (microseconds: Double) { self = microseconds * 1000 }
+    public init (milliseconds: Double) { self = milliseconds * 1e+6 }
+    public init (seconds: Double) { self = seconds * 1e+9 }
+    public init (minutes: Double) { self = minutes * 6e+10 }
+    public init (hours: Double) { self = hours * 3.6e+12 }
     public var microsecond: Double { return self / 1000 } 
     public var milliseconds: Double { return self / 1e+6 }
     public var seconds: Double { return self / 1e+9 } 
