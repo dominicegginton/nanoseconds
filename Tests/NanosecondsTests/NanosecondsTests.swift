a94db7304f6c5a3ede67789be0f40b04c5bed3d1
Dominic Egginton <dominic.egginton@gmail.com>
2021-01-06 18:00:21 +0000

    added timeinterval init mehtods to easy convert common time units (#2)

--- Tests/NanosecondsTests/NanosecondsTests.swift
+++ Tests/NanosecondsTests/NanosecondsTests.swift
@@ -7,4 +7,19 @@ final class NanosecondsTests: XCTestCase {
         let end = Nanoseconds()
         XCTAssertGreaterThan(end, start)
     }
+
+    func testTimeIntervalInitMethods () {
+        let nanosecond: TimeInterval = TimeInterval(nanoseconds: 1)
+        XCTAssertEqual(nanosecond, 1)
+        let microsecond: TimeInterval = TimeInterval(microseconds: 1)
+        XCTAssertEqual(microsecond, 1000)
+        let milliseconds: TimeInterval = TimeInterval(milliseconds: 1)
+        XCTAssertEqual(milliseconds, 1e+6)
+        let seconds: TimeInterval = TimeInterval(seconds: 1)
+        XCTAssertEqual(seconds, 1e+9)
+        let minutes: TimeInterval = TimeInterval(minutes: 1)
+        XCTAssertEqual(minutes, 6e+10)
+        let hours: TimeInterval = TimeInterval(hours: 1)
+        XCTAssertEqual(hours, 3.6e+12)
+    }
 }
