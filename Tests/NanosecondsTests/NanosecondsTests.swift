c0425bacf4694236dabe53bf8b3096f251484cbe
Dominic Egginton <dominic.egginton@gmail.com>
2021-03-27 15:27:55 +0000

    fix var names for consistency

--- Tests/NanosecondsTests/NanosecondsTests.swift
+++ Tests/NanosecondsTests/NanosecondsTests.swift
@@ -25,14 +25,14 @@ final class NanosecondsTests: XCTestCase {
         XCTAssertEqual(nanosecond, 1)
         let microsecond: TimeInterval = TimeInterval(microseconds: 1)
         XCTAssertEqual(microsecond, 1000)
-        let milliseconds: TimeInterval = TimeInterval(milliseconds: 1)
-        XCTAssertEqual(milliseconds, 1e+6)
-        let seconds: TimeInterval = TimeInterval(seconds: 1)
-        XCTAssertEqual(seconds, 1e+9)
-        let minutes: TimeInterval = TimeInterval(minutes: 1)
-        XCTAssertEqual(minutes, 6e+10)
-        let hours: TimeInterval = TimeInterval(hours: 1)
-        XCTAssertEqual(hours, 3.6e+12)
+        let millisecond: TimeInterval = TimeInterval(milliseconds: 1)
+        XCTAssertEqual(millisecond, 1e+6)
+        let second: TimeInterval = TimeInterval(seconds: 1)
+        XCTAssertEqual(second, 1e+9)
+        let minute: TimeInterval = TimeInterval(minutes: 1)
+        XCTAssertEqual(minute, 6e+10)
+        let hour: TimeInterval = TimeInterval(hours: 1)
+        XCTAssertEqual(hour, 3.6e+12)
     }
 
     func testTimeIntervalUnitConversion () {
